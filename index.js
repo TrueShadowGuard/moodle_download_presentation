e49128d648e7ee9818729460e10c8d1651ad9169
TrueShadowGuard <playvarian@gmail.com>
2022-11-04 12:06:09 +0300

    уменьшил ссылку

--- index.js
+++ index.js
@@ -1,1 +1,82 @@
-(()=>{window.require=null,window.define=null;const e=document.querySelector("h2")?.innerText?.replace(/Лекция[.]?/g,"")?.trim()||"безымянный.pdf",t=document.createElement("script");function n(e){return new Promise((function(t,n){const o=document.createElement("img");o.src="data:img/png;base64,"+e,o.onload=function(){t({w:o.width,h:o.height})}}))}t.src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js",t.async=!1,document.head.appendChild(t),t.onload=async()=>{const t=await async function(){return new Promise(((t,n)=>{console.log(1);const o=new XMLHttpRequest;document.body.className="",document.body.id="",document.body.innerHTML=`<progress id="indicator" style="width: 50%;"></progress><h1>Скачиваем ${e}</h1><div id="progress">Скачано: 0 Bytes</div>`;const d=document.querySelector("#progress"),r=document.querySelector("#indicator");o.onloadend=()=>{r.value="100",r.max="100",t(o.response)},o.onprogress=e=>{console.log(e),d.innerText="Скачано "+function(e,t=2){if(!+e)return"0 Bytes";const n=1024,o=t<0?0:t,d=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],r=Math.floor(Math.log(e)/Math.log(n));return`${parseFloat((e/Math.pow(n,r)).toFixed(o))} ${d[r]}`}(e.loaded)},o.open("GET",window.location.href,!0),o.send()}))}(),o=document.createElement("div");console.log(t),o.innerHTML=t;const d=[...o.querySelectorAll("slide")],r=d.map((e=>e.getAttribute("data"))),s=new jspdf.jsPDF;s.deletePage(1);const a=await Promise.all(r.map(n));d.map((e=>e.getAttribute("data"))).forEach(((e,t)=>{const{w:n,h:o}=a[t];s.addPage([n,o],"l"),s.addImage(e,null,0,0,n,o)}),s),s.output("save",e+".pdf")}})();
+(() => {
+    window.require = null;
+    window.define = null;
+
+    const filename = document.querySelector("h2")?.innerText?.replace(/Лекция[.]?/g, "")?.trim() || "безымянный.pdf";
+
+    const script = document.createElement("script");
+    script.src = "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js";
+    script.async = false;
+    document.head.appendChild(script);
+
+    script.onload = (async () => {
+        const htmlText = await downloadHtml();
+
+        const _document = document.createElement("div");
+        console.log(htmlText);
+        _document.innerHTML = htmlText;
+
+        const slides = [..._document.querySelectorAll("slide")];
+        const images = slides.map($slide => $slide.getAttribute("data"));
+
+        const doc = new jspdf.jsPDF();
+        doc.deletePage(1);
+
+        const imageSizes = await Promise.all(images.map(getImageDimensions));
+
+        slides
+            .map($slide => $slide.getAttribute("data"))
+            .forEach((base64img, i) => {
+                const {w, h} = imageSizes[i];
+                doc.addPage([w, h], "l")
+                doc.addImage(base64img, null, 0, 0, w, h);
+            }, doc);
+        doc.output('save', filename + '.pdf');
+    });
+
+    function getImageDimensions(base64) {
+        return new Promise(function (resolve, rejected) {
+            const img = document.createElement("img");
+            img.src = "data:img/png;base64," + base64;
+            img.onload = function () {
+                resolve({w: img.width, h: img.height})
+            };
+        })
+    }
+
+    async function downloadHtml() {
+        return new Promise((resolve, reject) => {
+            console.log(1);
+            const xhr = new XMLHttpRequest();
+            document.body.className = "";
+            document.body.id = "";
+            document.body.innerHTML = `<progress id="indicator" style="width: 50%;"></progress><h1>Скачиваем ${filename}</h1><div id="progress">Скачано: 0 Bytes</div>`;
+            const $progress = document.querySelector("#progress");
+            const $indicator = document.querySelector("#indicator");
+
+            xhr.onloadend = () => {
+                $indicator.value = "100";
+                $indicator.max = "100";
+                resolve(xhr.response);
+            }
+            xhr.onprogress = e => {
+                console.log(e);
+                $progress.innerText = "Скачано " + formatBytes(e.loaded);
+            }
+            xhr.open("GET", window.location.href, true);
+            xhr.send();
+        });
+    }
+
+    function formatBytes(bytes, decimals = 2) {
+        if (!+bytes) return '0 Bytes'
+
+        const k = 1024
+        const dm = decimals < 0 ? 0 : decimals
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+
+        const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+        return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+    }
+})();
---
+++ url.js
@@ -0,0 +1,5 @@
+(()=> {
+  let s = document.createElement("script");
+  s.src = "https://cdn.jsdelivr.net/gh/trueshadowguard/moodle_download_presentation/index.min.js";
+  document.head.append(s);
+})()
