236391a403441927f997a44ead4167354db9a6ce
TrueShadowGuard <playvarian@gmail.com>
2022-11-04 15:57:21 +0300

    добавил @main - указатель на ветку

--- url.js
+++ url.js
@@ -1,5 +1,5 @@
 (()=> {
   let s = document.createElement("script");
-  s.src = "https://cdn.jsdelivr.net/gh/trueshadowguard/moodle_download_presentation/index.min.js";
+  s.src = "https://cdn.jsdelivr.net/gh/trueshadowguard/moodle_download_presentation@main/index.min.js";
   document.head.append(s);
 })()
