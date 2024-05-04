1b6ed4d7b106a97a7251522f1b1cc59dea0c9c64
Vadim <playvarian@gmail.com>
2024-05-04 20:04:24 +0300

    remove old files

--- url.js
+++
@@ -1,5 +0,0 @@
-(()=> {
-  let s = document.createElement("script");
-  s.src = "https://cdn.jsdelivr.net/gh/trueshadowguard/moodle_download_presentation@main/index.min.js";
-  document.head.append(s);
-})()
